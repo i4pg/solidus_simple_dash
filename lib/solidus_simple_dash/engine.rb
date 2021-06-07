6603f835909498be4e9c86932aca2abc97236876
Sergey Pchelintsev <linz.sergey@gmail.com>
2021-06-07 17:35:18 +0300

    Improve simple dash UI

--- lib/solidus_simple_dash/engine.rb
+++ lib/solidus_simple_dash/engine.rb
@@ -13,17 +13,15 @@ module SolidusSimpleDash
       g.test_framework :rspec
     end
 
-    config.to_prepare do
-      if Spree.respond_to?(:solidus_version) && Spree.solidus_version > '1.4'
-        Spree::Backend::Config.configure do |config|
-          # This is the email submenu, useful for store users
-          config.menu_items << config.class::MenuItem.new(
-            [:overview],
-            'bar-chart',
-            label: 'overview',
-            condition: -> { can?(:manage, ::Spree::Overview) }
-          )
-        end
+    initializer 'solidus_simple_dash.add_admin_section' do
+      Spree::Backend::Config.configure do |config|
+        # This is the email submenu, useful for store users
+        config.menu_items << config.class::MenuItem.new(
+          [:overview],
+          'bar-chart',
+          label: 'overview',
+          condition: -> { can?(:manage, ::Spree::Overview) }
+        )
       end
     end
   end
